66a296fa83bf518e3fed695478983540f7d2e0f1
Paal Kristian Minne <paal.kristian.minne@gmail.com>
2018-06-02 21:16:34 +0200

    Kjørte prettier på server

--- part2/server.js
+++ part2/server.js
@@ -1,36 +1,37 @@
 const express = require("express");
 
 const app = express();
-var fs = require('fs');
+var fs = require("fs");
 
 // Middlewares
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+  res.sendFile("index.html", { root: __dirname });
 });
 
-
 // Data er hentet fra https://github.com/lsv/fifa-worldcup-2018
-var worldcupData = JSON.parse(fs.readFileSync('./worldcup2018.json', 'utf8'));
+var worldcupData = JSON.parse(fs.readFileSync("./worldcup2018.json", "utf8"));
 
-const groupMatches = Object.values(worldcupData.groups)
-  .reduce((allMatches, group) => allMatches.concat(Object.values(group.matches)), []);
+const groupMatches = Object.values(worldcupData.groups).reduce(
+  (allMatches, group) => allMatches.concat(Object.values(group.matches)),
+  [],
+);
 
-const knockoutMatches = Object.values(worldcupData.knockout)
-  .reduce((allMatches, round) => allMatches.concat(Object.values(round.matches)), []);
+const knockoutMatches = Object.values(worldcupData.knockout).reduce(
+  (allMatches, round) => allMatches.concat(Object.values(round.matches)),
+  [],
+);
 
 let matches = [...groupMatches, ...knockoutMatches];
 
 // let savedMatch = {
 //   matchId: 2,
-// } 
+// }
 let savedMatches = [];
 
-
 // Endepunkt
-
 
 app.get("/api/matches", (req, res) => {
   res.send({
@@ -55,11 +56,12 @@ app.get("/api/teams", (req, res) => {
 
 app.get("/api/savedmatches", (req, res) => {
   res.send({
-    savedMatches: savedMatches
+    savedMatches: savedMatches,
   });
 });
 
 app.post("/api/savedmatches", (req, res) => {
+  // Må håndtere slik at kamp ikke kan lagres to ganger
   if (!req.body.matchId) {
     res.status(400).send("Body with a matchId is required in request");
     return;
@@ -74,16 +76,15 @@ app.put("/api/savedmatches/:id", (req, res) => {
   let artist = tracks.find(a => a.id === parseInt(req.params.id));
   if (!artist) {
     res.status(404).send("The artist with the given ID was not found");
-    return
+    return;
   }
   if (!res.body.name || res.body.name.length < 3) {
-    res.status(400).send('Name is required and should be minimum 3 characters');
+    res.status(400).send("Name is required and should be minimum 3 characters");
     return;
   }
   artist.name = req.body.name;
   res.send(artist);
 });
-
 
 // PORT
 const port = process.env.PORT || 3000;
