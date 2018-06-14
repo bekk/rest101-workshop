b6d231d4c2d160f0a1bc4e80d0a7b7ae753398aa
Esben Aarseth <Esben.Aarseth@bekk.no>
2018-06-14 14:59:38 +0200

    Alle endepunkter returnerer 501 Not Implemented.

--- src/server.js
+++ src/server.js
@@ -46,133 +46,36 @@ app.get("/api/teams", (req, res) => {
   });
 });
 
-const decorateMatchWithLinks = (match) => {
-  return Object.assign({
-    _links: {
-      saveMatch: {
-        href: `/api/savedmatches/${match.name}`,
-        method: 'POST',
-        contentType: 'application/json',
-        accept: 'application/json'
-      },
-      deleteMatch: {
-        href: `/api/savedmatches/${match.name}`,
-        method: 'DELETE',
-        contentType: 'application/json',
-        accept: 'application/json'
-      },
-      information: {
-        href: `/api/matches/${match.name}`,
-        method: 'GET',
-        contentType: 'application/json',
-        accept: 'application/json'
-      }
-    }
-  }, match);
-};
-
 app.get("/api/matches", (req, res) => {
-  res.send({
-    matches: matches.map(decorateMatchWithLinks),
-  });
+  res.status(501).send({"message": "Not implemented yet"});
 });
 
 app.get("/api/matches/:id", (req, res) => {
-  const match = matches.find(m => m.name === parseInt(req.params.id));
-  if (!match) {
-    res.status(404).send("The match with the given ID was not found");
-    return;
-  }
-  res.send(match);
+  res.status(501).send({"message": "Not implemented yet"});
 });
 
 app.get("/api/savedmatches", (req, res) => {
-  res.send({
-    savedMatches: savedMatches,
-  });
+  res.status(501).send({"message": "Not implemented yet"});
 });
 
-const APPLICATION_JSON_V1 = 'application/vnd.fotballfest.v1+json';
-
 app.get('/api/channels/:id', (req, res) => {
-  const channel = channels.filter(c => {
-    return c.id == req.params.id;
-  })[0];
-  if (req.header('Accept') === APPLICATION_JSON_V1) {
-    res.send({name: channel.name, icon: channel.icon});
-  } else {
-    res.send(channel);
-  }
+  res.status(501).send({"message": "Not implemented yet"});
 });
 
 app.get('/api/weather/', (req, res) => {
-  if (!req.query.time) {
-    res.status(400).send();
-    return;
-  }
-  const weatherUrl = "https://fotballfest-test.herokuapp.com/api/weather?time=" + encodeURIComponent(req.query.time);
-  fetch(weatherUrl).then(res => res.json()).then(weather => {
-    res.send(weather);
-  });
+  res.status(501).send({"message": "Not implemented yet"});
 });
 
-const matchAlreadyExists = (matchId) => {
-  return savedMatches.filter(match => match.matchId === matchId).length > 0;
-};
-
 app.post("/api/savedmatches", (req, res) => {
-  if (!req.body.matchId) {
-    res.status(400).send("Body with a matchId is required in request");
-    return;
-  }
-
-  if (matchAlreadyExists(req.body.matchId)) {
-    res.status(400).send("Match already exists");
-    return;
-  }
-
-  let matchId = req.body.matchId;
-
-  const newSavedMatch = {
-    matchId: matchId,
-    kontraskjaeret: false,
-  };
-  savedMatches.push(newSavedMatch);
-  res.send(newSavedMatch);
+  res.status(501).send({"message": "Not implemented yet"});
 });
 
 app.delete("/api/savedmatches/:id", (req, res) => {
-  const indexOfMatch = savedMatches
-    .map(e => e.matchId)
-    .indexOf(parseInt(req.params.id, 10));
-  if (indexOfMatch === -1) {
-    res.status(404).send();
-  } else {
-    savedMatches.splice(indexOfMatch, 1);
-    res.status(204).send();
-  }
+  res.status(501).send({"message": "Not implemented yet"});
 });
 
 app.put("/api/savedmatches/:id", (req, res) => {
-  const indexOfMatch = savedMatches
-    .map(e => e.matchId)
-    .indexOf(parseInt(req.params.id, 10));
-  if (indexOfMatch < 0) {
-    res
-      .status(404)
-      .send(
-        `The match with id=${
-          req.params.id
-        } was not in the list of saved matches.`
-      );
-    return;
-  }
-  if (!req.body) {
-    res.status(400).send("Body is required");
-    return;
-  }
-  savedMatches[indexOfMatch] = req.body;
-  res.send(req.body);
+  res.status(501).send({"message": "Not implemented yet"});
 });
 
 // PORT
